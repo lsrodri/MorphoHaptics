45d161d8384559b52fdd7fc48a8e86185c4c2dc5
Lucas <lrodri@terpmail.umd.edu>
2025-05-17 11:52:17 +0200

    Migrate keyCallback to separate InputCallbacks

--- src/UI/InputCallbacks.cpp
+++ src/UI/InputCallbacks.cpp
@@ -42,3 +42,124 @@ void mouseScrollCallback(GLFWwindow* a_window, double a_offsetX, double a_offset
 }
 
 //------------------------------------------------------------------------------
+
+void keyCallback(GLFWwindow* a_window, int a_key, int a_scancode, int a_action, int a_mods)
+{
+
+    // filter calls that only include a key press
+    if ((a_action != GLFW_PRESS) && (a_action != GLFW_REPEAT))
+    {
+        return;
+    }
+
+    // option - exit
+    else if ((a_key == GLFW_KEY_ESCAPE) || (a_key == GLFW_KEY_Q))
+    {
+        glfwSetWindowShouldClose(a_window, GLFW_TRUE);
+    }
+
+    else if (a_key == GLFW_KEY_DOWN)
+    {
+        updateValueHapticsRadius(-1);
+    }
+
+    else if (a_key == GLFW_KEY_UP)
+    {
+        updateValueHapticsRadius(1);
+    }
+
+    // polygonize model and save to file
+    else if (a_key == GLFW_KEY_M)
+    {
+        startPolygonize();
+    }
+
+    // load dataset
+    else if (a_key == GLFW_KEY_L)
+    {
+        loadDataset();
+    }
+
+    // toggle fullscreen
+    else if (a_key == GLFW_KEY_F)
+    {
+        // toggle state variable
+        fullscreen = !fullscreen;
+
+        // get handle to monitor
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+
+        // get information about monitor
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+        // set fullscreen or window mode
+        if (fullscreen)
+        {
+            glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+            glfwSwapInterval(swapInterval);
+        }
+        else
+        {
+            int w = 0.8 * mode->height;
+            int h = 0.5 * mode->height;
+            int x = 0.5 * (mode->width - w);
+            int y = 0.5 * (mode->height - h);
+            glfwSetWindowMonitor(window, NULL, x, y, w, h, mode->refreshRate);
+            glfwSwapInterval(swapInterval);
+        }
+    }
+
+    else if (a_key == GLFW_KEY_G)
+    {
+        object->setGhostEnabled(!object->getGhostEnabled());
+    }
+
+    else if (a_key == GLFW_KEY_H)
+    {
+        toggleHaptics();
+    }
+
+    else if (a_key == GLFW_KEY_S)
+    {
+        toggleSculpting();
+    }
+
+    else if (a_key == GLFW_KEY_K)
+    {
+        toggleVoxelValueHaptics();
+    }
+
+    else if (a_key == GLFW_KEY_X)
+    {
+        toggleForceSmoothing();
+    }
+
+    else if (a_key == GLFW_KEY_SPACE)
+    {
+        toggleGhostMode();
+    }
+
+    else if (a_key == GLFW_KEY_V)
+    {
+        startExportVolume();
+    }
+
+    else if (a_key == GLFW_KEY_TAB)
+    {
+        isUILayerVisible = !isUILayerVisible;
+        panel->setShowEnabled(isUILayerVisible);
+    }
+
+    else if (a_key == GLFW_KEY_EQUAL || a_key == GLFW_KEY_KP_ADD)
+    {
+        updateProbeRadius(1);
+    }
+
+    else if (a_key == GLFW_KEY_MINUS || a_key == GLFW_KEY_KP_SUBTRACT)
+    {
+        updateProbeRadius(-1);
+    }
+
+}
+
+//------------------------------------------------------------------------------
--- src/UI/InputCallbacks.h
+++ src/UI/InputCallbacks.h
@@ -7,6 +7,28 @@ extern MouseStates mouseState;
 extern double mouseX, mouseY;
 extern chai3d::cCamera* camera;
 extern chai3d::cToolCursor* tool[];
+extern bool fullscreen;
+extern GLFWwindow* window;
+extern int swapInterval;
+extern chai3d::cVoxelObject* object;
+extern bool isUILayerVisible;
+extern chai3d::cPanel* panel;
+extern chai3d::cLabel* button1, * button2, * button3, * button4, * button5, * button6, * button7, * button8, * button9, * button10, * button11, * button12, * button13;
+extern chai3d::cLabel* labelRates;
+extern chai3d::cLabel* statusMessage;
+extern int toolOne, toolTwo;
+
+void startPolygonize();
+void loadDataset();
+void toggleHaptics();
+void toggleSculpting();
+void toggleVoxelValueHaptics();
+void toggleForceSmoothing();
+void toggleGhostMode();
+void startExportVolume();
+void updateValueHapticsRadius(int delta);
+void updateProbeRadius(int delta);
 
 void mouseMotionCallback(GLFWwindow* a_window, double a_posX, double a_posY);
 void mouseScrollCallback(GLFWwindow* a_window, double a_offsetX, double a_offsetY);
+void keyCallback(GLFWwindow* a_window, int a_key, int a_scancode, int a_action, int a_mods);
